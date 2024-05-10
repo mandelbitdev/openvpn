4a4adce7bbb80c28548d3c30d862d6208f3244aa
Gianmarco De Gregori <gianmarco@mandelbit.com>
2024-05-10 11:49:30 +0200

    Ensures all params are ready before invoking dco_set_peer()
    In UDP case the dco_set_peer() is currently perfomed at the wrong time
    since the mssfix param is calculated later on in
    tls_session_update_crypto_params_do_work().
    By moving the dco_set_peer() inside the
    tls_session_update_crypto_params_do_work() and removing
    the p2p_set_dco_keepalive() otherwise on client side the
    dco_set_peer() will be called twice, we will ensure
    that all crypto and frame params are properly initialized
    and if an update occurs dco will be notified.
    
    Change-Id: Ic8538e734dba53cd43fead3961e4401c8037e079
    Signed-off-by: Gianmarco De Gregori <gianmarco@mandelbit.com>

--- src/openvpn/ssl.c
+++ src/openvpn/ssl.c
@@ -1584,7 +1584,8 @@ tls_session_update_crypto_params_do_work(struct tls_multi *multi,
                                          struct options *options,
                                          struct frame *frame,
                                          struct frame *frame_fragment,
-                                         struct link_socket_info *lsi)
+                                         struct link_socket_info *lsi,
+                                         dco_context_t *dco)
 {
     if (session->key[KS_PRIMARY].crypto_options.key_ctx_bi.initialized)
     {
@@ -1631,6 +1632,26 @@ tls_session_update_crypto_params_do_work(struct tls_multi *multi,
             return false;
         }
     }
+
+    if (dco_enabled(options))
+    {
+        /* dco_set_peer() must be called only when both
+         * keepalive and mss_fix are properly set. */
+        if (options->ping_send_timeout || frame->mss_fix)
+        {
+            int ret = dco_set_peer(dco,
+                                   multi->dco_peer_id,
+                                   options->ping_send_timeout,
+                                   options->ping_rec_timeout,
+                                   frame->mss_fix);
+            if (ret < 0)
+            {
+                msg(D_DCO, "Cannot set DCO peer parameters for peer (id=%u): %s",
+                    multi->dco_peer_id, strerror(-ret));
+                return false;
+            }
+        }
+    }
     return tls_session_generate_data_channel_keys(multi, session);
 }
 
@@ -1639,7 +1660,8 @@ tls_session_update_crypto_params(struct tls_multi *multi,
                                  struct tls_session *session,
                                  struct options *options, struct frame *frame,
                                  struct frame *frame_fragment,
-                                 struct link_socket_info *lsi)
+                                 struct link_socket_info *lsi,
+                                 dco_context_t *dco)
 {
     if (!check_session_cipher(session, options))
     {
@@ -1650,7 +1672,7 @@ tls_session_update_crypto_params(struct tls_multi *multi,
     session->opt->crypto_flags |= options->imported_protocol_flags;
 
     return tls_session_update_crypto_params_do_work(multi, session, options,
-                                                    frame, frame_fragment, lsi);
+                                                    frame, frame_fragment, lsi, dco);
 }
 
 
