ecc8e4217ac4528bc0eecf0fae391bcb6e05e804
Gianmarco De Gregori <gianmarco@mandelbit.com>
2024-07-30 15:40:22 +0200

    Add support to "ovpn" aka "in upstream kernel"
    Introduce new userspace APIs to communicate with "ovpn".
    Besides various changes to commands and attributes, the main
    difference is that the creation and destruction of network
    devices will be done via Netlink insted of SITNL.
    
    Signed-off-by: Gianmarco De Gregori <gianmarco@mandelbit.com>

--- src/openvpn/dco_linux.h
+++ src/openvpn/dco_linux.h
@@ -31,6 +31,10 @@
 #include <netlink/socket.h>
 #include <netlink/netlink.h>
 
+/* Defines to avoid mismatching with other platforms */
+#define OVPN_CMD_DEL_PEER OVPN_CMD_PEER_DEL_NTF
+#define OVPN_CMD_SWAP_KEYS OVPN_CMD_KEY_SWAP_NTF
+
 typedef enum ovpn_key_slot dco_key_slot_t;
 typedef enum ovpn_cipher_alg dco_cipher_t;
 
@@ -50,6 +54,7 @@ typedef struct
 
     int dco_message_type;
     int dco_message_peer_id;
+    int dco_message_key_id;
     int dco_del_peer_reason;
     uint64_t dco_read_bytes;
     uint64_t dco_write_bytes;
